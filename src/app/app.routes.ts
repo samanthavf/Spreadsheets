12cb5a0d22ed6f44034ae57bfc258aacd5fff6c8
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-10-10 23:35:38 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/spreadsheet', pathMatch: 'full' }, // Redireciona para o componente
+    { path: 'spreadsheet', component: HomeComponentComponent}
+];
