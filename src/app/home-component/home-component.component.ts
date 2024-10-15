a5000ec019e4472bf82b2d27b1b82fa9331c95fd
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-10-15 14:51:40 -0300

    pdf commit

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,12 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { Cell } from './cell';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
+
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css'
 })
 export class HomeComponentComponent {
+apresentacao:boolean = true;
+tabela:boolean=false;
+rows: Cell [][]= [[{value:''}]];
 
+addRow() {
+  const newRow = this.rows[0].map(() => ({ value: '' }));
+  this.rows.push(newRow);
+}
+
+
+addColumn() {
+  this.rows.forEach(row => row.push({ value: '' }));
+}
+
+
+removeRow() {
+  if (this.rows.length > 1) {
+    this.rows.pop();
+  }
+}
+
+
+removeColumn() {
+  if (this.rows[0].length > 1) {
+    this.rows.forEach(row => row.pop());
+  }
+}
+
+planilhas() {
+  this.apresentacao = false;
+  this.tabela = true;
+  }
+
+  downloadPDF() {
+    const doc = new jsPDF();
+    const table = document.querySelector('table');
+    if (table) {
+      doc.autoTable({ html: table });
+      doc.save('planilha.pdf');
+    }
+  }
 }
